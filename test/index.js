a2e196d1286fb3abf5fc95e342e67792f7ab42e6
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-22 13:17:12 +0200

    Specify `dr-promise/custom-promise`

--- test/index.js
+++ test/index.js
@@ -1,3 +1,7 @@
 require('../').then(function () {
-  document.body.innerHTML = '<h1>DomReady!</h1>';
+  document.body.innerHTML += '<h1>DomReady!</h1>';
+});
+
+require('../custom-promise')(require('es6-promise').Promise).then(function () {
+  document.body.innerHTML += '<h1>custom-promise: DomReady!</h1>';
 });
